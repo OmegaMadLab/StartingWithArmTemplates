7eb2a6d1a281ef875dc10193c3bcbd6c3c151059
Marco Obinu <locurtola@tin.it>
2018-11-24 08:51:17 +0100

    SqlSat777 update

--- 06_CSE_example/deploy.ps1
+++ 06_CSE_example/deploy.ps1
@@ -52,3 +52,32 @@ $Rg = Set-AzureRg -Name $RgName
 New-AzResourceGroupDeployment -ResourceGroupName $Rg.ResourceGroupName `
     -TemplateFile (Get-Item .\CSE-sqlConfig.json).FullName `
     -TemplateParameterFile (Get-Item .\CSE-sqlConfig.parameters.json).FullName
+
+
+#### SQL Example - DSC resource "hack" to launch a custom script
+### Example 1
+#Deploy a VM which will execute the "hacked" DSC
+$RgName = "ArmDemo-SqlIaasIntegration4-RG"
+$Rg = Set-AzureRg -Name $RgName
+
+New-AzResourceGroupDeployment -ResourceGroupName $Rg.ResourceGroupName `
+    -TemplateFile (Get-Item .\newSqlVm-dscAsCustomScriptApplied.json).FullName `
+    -TemplateParameterFile (Get-Item .\newSqlVm-dscAsCustomScriptApplied.parameters.json).FullName `
+    -AsJob
+
+# Deploy a SQL VM
+$RgName = "ArmDemo-SqlIaasIntegration3-RG"
+$Rg = Set-AzureRg -Name $RgName
+
+New-AzResourceGroupDeployment -ResourceGroupName $Rg.ResourceGroupName `
+    -TemplateFile (Get-Item .\newSqlVm.json).FullName `
+    -TemplateParameterFile (Get-Item .\newSqlVm.parameters.json).FullName `
+    -AsJob
+
+## Deploy the CSE resource on an existing VM - Using one of the VM generated during previous examples
+$RgName = "ArmDemo-SqlIaasIntegration3-RG"
+$Rg = Set-AzureRg -Name $RgName
+
+New-AzResourceGroupDeployment -ResourceGroupName $Rg.ResourceGroupName `
+    -TemplateFile (Get-Item .\CSE-sqlConfig.json).FullName `
+    -TemplateParameterFile (Get-Item .\CSE-sqlConfig.parameters.json).FullName
